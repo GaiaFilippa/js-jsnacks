5641c31c4aa9a118507c8427cfb8acfff4661f23
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-04-15 16:51:49 +0200

    jsnack3 not complete

---
+++ jsnack3/script.js
@@ -0,0 +1,20 @@
+// JSNACK3
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+
+/* for(let i=0; i<10; i++){
+    let userNumbers = prompt("Inserisci un numero: ");
+    console.log(userNumbers);
+
+    let isUserNumberANumber = !isNaN(userNumbers);
+
+    if (isUserNumberANumber) {
+        userNumbers = parseInt(userNumbers);
+    } else {
+        console.log("Gli input inseriti non sono validi, riesegui il programma!");
+    }
+
+
+} */
+
